fcd4f8d99e2cdc5a6c328b7231cad07187d00e7f
mobinghane <149666789+mobinghane@users.noreply.github.com>
2024-01-27 21:53:51 +0330

    Update mehmani.py

--- py1/mehmani.py
+++ py1/mehmani.py
@@ -1,6 +1,6 @@
-q=list(bin(int(input()))[2::])
-w=list(bin(int(input()))[2::])
-e=int(input())
+q = list(bin(int(input()))[2::])
+w = list(bin(int(input()))[2::])
+e = int(input())
 del q[0]
 del q[1]
 del w[0]
@@ -9,12 +9,12 @@ while len(q)<32:
      q.insert(0,'0')
 while len(w)<32:
      w.insert(0,'0')
-r=w+q
-t=[]
-for i in range (1,e+1):
+r = w + q
+t = []
+for i in range (1, e+1):
      t.append(input())
 for i in t:
-     if r[63-int(i)]== '0':
+     if r[63-int(i)] == '0':
          print('no')
      else:
          print('yes')     
