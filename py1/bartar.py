eb80e9f400ad473600577531c56624ce693396fc
mobinghane <149666789+mobinghane@users.noreply.github.com>
2024-01-27 21:52:22 +0330

    Update bartar.py

--- py1/bartar.py
+++ py1/bartar.py
@@ -1,22 +1,21 @@
-q,w=map(int,input().split())
-if q>w:
-  x=range(q,w+1)
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+q, w = map(int, input().split())
+
+if q > w:
+    x = range(w, q + 1)
 else:
-  x=range(w,q+1)
-e =[]
-for i in x:
-  e.append(i)
-  for j in range(2,i):
-    if i%j==0:
-     e.remove(i)
-     break
-for k in e:
-  if k == 0:
-    e.remove(k)
-for k in e:
-  if k == 1:
-    e.remove(k)
-if w>=q:
-  print("main order - amount:",len(e))
+    x = range(q, w + 1)
+
+e = [num for num in x if is_prime(num)]
+
+if w >= q:
+    print("main order - amount:", len(e))
 else:
-  print("reverse order - amount:",len(e))
+    print("reverse order - amount:", len(e))
